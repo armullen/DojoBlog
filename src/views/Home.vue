4bb2bb3bad5b48424552f1adb1d693c273599581
Amber Mullen <121705867+armullen@users.noreply.github.com>
2023-09-19 15:54:52 -0500

    learned commuted propery

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,70 @@
-<template>
+<!-- <template>
   <div class="home">
-    <p>home</p>
-    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick">Click me</button>
+    <h1>Home</h1>
+    <h2>Refs</h2>
+    <p>{{  ninjaOne.name }} - {{ ninjaOne.age }}</p>
+    <button @click="updateNinjaOne">Update Ninja One</button>
+    <h2>Reactive</h2>
+    <p>{{  ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
+    <button @click="updateNinjaTwo">Update Ninja Two</button>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 
 export default {
   name: 'Home',
   setup() {
-    console.log('setup')
+    const ninjaOne = ref({ name: 'Mario', age: 25 })
+    const ninjaTwo = reactive({ name: 'Luigi', age: 35}) 
 
-    const p = ref(null)
-    console.log(p, p.value)
+    const updateNinjaOne = () => {
+      ninjaOne.value.age = 30
+    }
 
-    let name = 'Mario'
-    let age = 25
-
-    const handleClick = () => {
-      console.log(p, p.value)
-      p.value.classList.add('test')
-      p.value.textContent = 'hello, ninjas'
+    const updateNinjaTwo = () => {
+      ninjaTwo.age = 45
     }
 
     return {
-      name: name,
-      age: age,
-      handleClick,
-      p
+      ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo
     }
   },
 }
 
+</script> -->
+
+
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <input type="text" v-model="search">
+    <p>search term - {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
+  </div>
+</template>
+
+<script>
+import { computed, ref } from 'vue'
+
+export default {
+  name: 'Home',
+  setup() {
+    const search = ref('')
+    const names = ref(['Mario', 'Luigi', 'Yoshi', 'Koopa', 'Peach', 'Bowser', 'Toad'])
+
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => {
+        return name.toLowerCase().includes(search.value.toLowerCase())
+      })
+    })
+
+    return {
+       names,
+       search,
+       matchingNames 
+    }
+  }
+}
 </script>
